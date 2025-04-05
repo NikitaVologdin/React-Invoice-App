dfe07eb20adb059ccf6509e198339fe3c39041c1
Nikita Vologdins <nvologdins@gmail.com>
2025-04-05 16:54:00 +0300

    resolving path for font

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  assetsInclude: ["**/*.woff2"],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+          router: ["react-router"],
+        },
+      },
+    },
+  },
 });
