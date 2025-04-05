6ca2c880ed0c12a86cc03b3b05522da909d12a0d
Nikita Vologdins <nvologdins@gmail.com>
2025-04-05 21:29:53 +0300

    resolved new page bg issue

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -1,10 +1,19 @@
+import { useLocation } from "react-router";
+
 type props = {
   children: React.ReactNode;
 };
 
 export default function Container({ children }: props) {
+  const location = useLocation();
+  const arr = location.pathname.split("/");
+  const isNew = arr.includes("new");
+  const bg = isNew ? "bg-white" : "bg-[var(--blue-white)]";
+
   return (
-    <div className="relative flex flex-col px-6 md:px-12 bg-[var(--blue-white)] min-h-screen dark:bg-[var(--deep-black)]">
+    <div
+      className={`relative flex flex-col px-6 md:px-12 ${bg}  min-h-screen dark:bg-[var(--deep-black)]`}
+    >
       {children}
     </div>
   );
