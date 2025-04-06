3b1d30569960ea1fad8ec0070e4cb1167ce58e40
Nikita Vologdins <nvologdins@gmail.com>
2025-04-06 22:03:23 +0300

    moved authCheck to the useEffect

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useAppDispatch } from "../hooks/redux/hooks";
 import { showNotification } from "../store/features/notification/notificationSlice";
+import { useEffect } from "react";
 
 type props = {
   children: React.ReactNode;
@@ -11,11 +12,15 @@ export default function ProtectedRoute({ children }: props) {
   const auth = getAuth();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      dispatch(showNotification("Please login"));
-      navigate("/login");
-    }
-  });
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        dispatch(showNotification("Please login"));
+        navigate("/login");
+      }
+    });
+  }, [auth, dispatch, navigate]);
+
   return <>{children}</>;
 }
