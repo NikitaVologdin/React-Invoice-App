4b9bfe3659b140752ed8c742434541747ef88c34
Nikita Vologdins <nvologdins@gmail.com>
2025-04-07 17:12:09 +0300

    added dark mode for header

--- src/ui/invoices/NoInvoices.tsx
+++ src/ui/invoices/NoInvoices.tsx
@@ -4,7 +4,7 @@ export default function NoInvoices() {
   return (
     <div className="h-full flex flex-col justify-center max-w-[242px] mt-[102px] md:mt-[154px] lg:mt-[85px]">
       <img src={empty} alt="" width={242} height={200} />
-      <h1 className="text-2xl text-[var(--A08)] font-bold mt-[42px]">
+      <h1 className="text-2xl text-[var(--A08)] font-bold mt-[42px] dark:text-white">
         There is nothing here
       </h1>
       <p className="text-[var(--A06)] text-center mt-[23px]">
